200fee1872e43c38af653e7374706eb35b2d414b
Edwin Anaya <eanaya@lean-tech.io>
2019-10-07 18:12:10 -0500

    feat: config db

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
     port: process.env.PORT || 80,
-    db: process.env.MONGODB_URI || 'mongodb://localhost:2027/backendrealsoccer',
+    db: process.env.MONGODB_URI || 'mongodb+srv://edwik:95091310024@realsoccer-spksl.gcp.mongodb.net/admin?retryWrites=true&w=majority',
     //db: process.env.MONGODB || 'mongodb://190.144.179.82:2027/mockserverdb',
     SECRET_TOKEN: 'mysecrettoken$2019'
 }
