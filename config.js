91c3af230ef928b23eb23864d0243be79ba59d0d
Edwin Anaya <eanaya@lean-tech.io>
2019-10-07 18:15:38 -0500

    feat: change port

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    port: process.env.PORT || 80,
+    port: process.env.PORT || 3001,
     db: process.env.MONGODB_URI || 'mongodb+srv://edwik:95091310024@realsoccer-spksl.gcp.mongodb.net/admin?retryWrites=true&w=majority',
     //db: process.env.MONGODB || 'mongodb://190.144.179.82:2027/mockserverdb',
     SECRET_TOKEN: 'mysecrettoken$2019'
