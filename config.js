08be7ca21fc2579b22ed36ce33dc87d179972372
Edwin Anaya <eanaya@lean-tech.io>
2019-10-07 17:53:35 -0500

    feat: change name MONGODB_URI

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
     port: process.env.PORT || 80,
-    db: process.env.MONGODB_URL || 'mongodb://localhost:2027/backendrealsoccer',
+    db: process.env.MONGODB_URI || 'mongodb://localhost:2027/backendrealsoccer',
     //db: process.env.MONGODB || 'mongodb://190.144.179.82:2027/mockserverdb',
     SECRET_TOKEN: 'mysecrettoken$2019'
 }
